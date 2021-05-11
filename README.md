4b89d31988ac678b06d49d1f2009fa94946598ae
garytate123 <garytatelufc@gmail.com>
2021-05-11 14:09:19 +0100

    updated ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ONLINE MENTORING APP
+
+Start the project with npm start
+
+### Created:
+
+- Home page
+- Sign up page
+- Login page
+- Nav component
+
+### Need to:
+
+- Style Home page
+- Style Navbar
+- Create and style sign up form
+- Create and style login form
+- Create a welcome page for when someone is logged in
+- Create user database
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,17 +4,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from './Homepage';
 import Signup from './Signup-page';
 import Login from './Login-page';
-import Nav from './Nav'
+import Nav from './Nav';
 
 function App() {
     return (
         <Router>
-        <Nav />
-            <Switch>
-                <Route exact path="/" component={Homepage}/>
-                <Route exact path="/Signup" component={Signup}/>
-                <Route exact path="/Login" component={Login}/>
-             </Switch>
+            <Nav />
+                <Switch>
+                    <Route exact path="/">
+                        <Homepage />
+                    </Route>
+                    <Route exact path="/Signup">
+                        <Signup />
+                    </Route>
+                     <Route exact path="/Login">
+                        <Login />
+                    </Route>
+                </Switch>
         </Router>
 
     );
