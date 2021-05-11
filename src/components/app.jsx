c2eecd679773573b1884f65c41edb2bcf76d8578
garytate123 <garytatelufc@gmail.com>
2021-05-11 13:20:00 +0100

    added react-router-dom

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Homepage from './Homepage';
+// import Signup from './Signup/Signup-page';
 
 function App() {
-    return <div>
-        <h1>Hello There!</h1>   
-    </div>
+    return (
+        <Router>
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </Router>
+
+    );
 }
 
 export default App;
